de8881149ae18f23aabca19fbb09536547f6bf37
Derick Arzu <arzuderick@unitec.edu>
2018-05-11 06:32:29 -0600

    Could show names

--- js/Departments.jsx
+++ js/Departments.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-// import deptsJson from '../../May11_Homework1/data.json';
+import deptsJson from '../../May11_Homework1/data.json';
 
-// const deptsArray = deptsJson.departments;
+const deptsArray = deptsJson.departments;
 
-const Departments = () => (
+const htmlContent = (
   <div className="departments">
-    <h1>Departamentos</h1><br />
-    <body>
-      <p>Reaction</p>
-    </body>
+    <center>
+      <h1>Departamentos</h1><br />
+      <tbody>
+        {deptsArray[0].name} <br />
+        {deptsArray[1].name} <br />
+        {deptsArray[2].name} <br />
+        {deptsArray[3].name} <br />
+        {deptsArray[4].name} <br />
+        {deptsArray[5].name} <br />
+      </tbody>
+    </center>
   </div>
 );
+
+const Departments = () => htmlContent;
 export default Departments;
