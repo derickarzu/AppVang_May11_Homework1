7382828ca4c55b18b15cd8b9e9a29396ad959fdc
Derick Arzu <arzuderick@unitec.edu>
2018-05-11 15:57:56 -0600

    Solved part of the issue

--- js/Department.jsx
+++ js/Department.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 
 import deptsJson from '../../May11_Homework1/data.json';
 
-function findDepartment(name) {
-  return deptsJson.find(dept => dept.name === name);
-}
+const deptsArray = deptsJson.departments;
 
-const Department = props => {
+/* function findDepartment(name) {
+  return deptsJson.find(dept => dept.name === name);
+} */
+
+/* const Department = props => {
   const department = findDepartment(props.params.name);
   return (
     <div id="info">
       {department}
     </div>
   );
-};
+}; */
+// const department = findDepartment('Cortés');
+const Department = () => (
+  <div id="info">
+    <h1>{deptsArray[0].name}</h1>
+  </div>
+);
 
 export default Department;
