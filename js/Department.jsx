a61de0a9f16ff763cf2155d066f06b4a4376ec57
Derick Arzu <arzuderick@unitec.edu>
2018-05-11 05:48:59 -0600

    Could import JSON

--- js/Department.jsx
+++ js/Department.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const Department = () => <div className="department" />;
+const Department = () => (
+  <div>
+    <h1>Departmentos</h1>
+  </div>
+);
 
 export default Department;
--- js/Departments.jsx
+++ js/Departments.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+// import deptsJson from '../../May11_Homework1/data.json';
 
-const Departments = () => <div className="departments" />;
+// const deptsArray = deptsJson.departments;
 
+const Departments = () => (
+  <div className="departments">
+    <h1>Departamentos</h1><br />
+    <body>
+      <p>Reaction</p>
+    </body>
+  </div>
+);
 export default Departments;
